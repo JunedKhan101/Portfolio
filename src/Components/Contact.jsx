92eb23ca6cab95934d642fb8a90631b4f4ab8972
JunedKhan101 <k.sonuc101@gmail.com>
2021-06-17 21:20:47 +0530

    Improved messages in contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { send } from 'emailjs-com';
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Form, Button, Container, Row, Col, Alert } from "react-bootstrap";
 import { SocialIcon } from 'react-social-icons';
 import "../css/contact.css";
 require('dotenv').config();
@@ -12,6 +12,7 @@ export default function Contact() {
 	    email: '',
 	    message: '',
   	});
+  	const [bool, setBool] = useState(undefined);
   	const handleChange = (e) => {
     	setToSend({ ...toSend, [e.target.name]: e.target.value });
   	};
@@ -24,32 +25,22 @@ export default function Contact() {
 	      process.env.REACT_APP_USERID
 	    )
 	    .then((response) => {
+	      // alert("Message sent successfully!");
+	      setBool(true);
 	      console.log('SUCCESS!', response.status, response.text);
 	    })
 	    .catch((err) => {
+	      // alert("Something went wrong :(");
+	      setBool(false);
 	      console.log('FAILED.', err);
 	    });
   	};
-	return (
-		<div className="contact-container" id="contact">
-			<h1 className="heading">Contact</h1>
-			<Container className="contact">
-			<Row>
-			<Col>
-				<div className="social-media">
-					<h2 className="heading">Let's Chat!</h2>
-					<div className="icons">
-						<SocialIcon target="_blank" url="https://www.linkedin.com/in/juned-khan-22b041204/" />
-						<SocialIcon target="_blank" url="https://twitter.com/JunedK101" />
-						<SocialIcon target="_blank" url="https://www.instagram.com/its_juned42/" />
-						<SocialIcon target="_blank" url="https://www.facebook.com/JunedKhanc101" />
-					</div>
-				</div>
-			</Col>
-			<Col>
-				<div className="form-container">
-					<h2 className="heading">Send me a message</h2>
-					<form onSubmit={onSubmit} className="form">
+  	const renderForm = () => {
+  		if (bool === undefined) {
+  			return (
+  				<div>
+  				<h2 className="heading">Send me a message</h2>
+  				<form onSubmit={onSubmit} className="form">
 						<Form.Group controlId="name">
 							<Form.Label>Name</Form.Label>
 						    <Form.Control required name="name" onChange={handleChange} type="text" />
@@ -68,6 +59,45 @@ export default function Contact() {
 						</Form.Group>
 						<Button variant="dark" type="submit">Submit</Button>
 					</form>
+				</div>
+  			);
+  		}
+  		else if (bool === true) {
+  			return (
+  				<div className="form-msg">
+	  				<h2 className="heading success">Message sent successfully!</h2>
+	  				<Button className="back-btn" onClick={() => setBool(undefined)} variant="dark">Back</Button>
+  				</div>
+  			);	
+  		}
+  		else {
+  			return (
+  				<div className="form-msg">
+	  				<h2 className="heading fail">Oops! Something went wrong :(</h2>
+	  				<Button className="back-btn" onClick={() => setBool(undefined)} variant="dark">Back</Button>
+  				</div>
+  			);
+  		}
+  	}
+	return (
+		<div className="contact-container" id="contact">
+			<h1 className="heading">Contact</h1>
+			<Container className="contact">
+			<Row>
+			<Col>
+				<div className="social-media">
+					<h2 className="heading">Let's Chat!</h2>
+					<div className="icons">
+						<SocialIcon target="_blank" url="https://www.linkedin.com/in/juned-khan-22b041204/" />
+						<SocialIcon target="_blank" url="https://twitter.com/JunedK101" />
+						<SocialIcon target="_blank" url="https://www.instagram.com/its_juned42/" />
+						<SocialIcon target="_blank" url="https://www.facebook.com/JunedKhanc101" />
+					</div>
+				</div>
+			</Col>
+			<Col>
+				<div className="form-container">
+					{renderForm()}
 				</div>
 			</Col>
 				</Row>
