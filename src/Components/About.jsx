0674c5500810955992a0973783ae33a2ea72bf12
JunedKhan101 <k.sonuc101@gmail.com>
2021-07-28 21:45:46 +0530

    Fixed the ul list item alignment

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,9 +5,9 @@ export default function About() {
   return (
 	<div className="about" id="about">
 		<h2>About Myself</h2>
-		<p>As a Developer, I've committed myself to lifelong learning.</p>
-		<p>In my college life, I became passionate about computers and how they work.</p>
-		<p>I am curious about new and promising technologies such as AI, Machine Learning and Blockchain</p>
+		<p className="description">As a Developer, I've committed myself to lifelong learning.</p>
+		<p className="description">In my college life, I became passionate about computers and how they work.</p>
+		<p className="description">I am curious about new and promising technologies such as AI, Machine Learning and Blockchain</p>
 	</div>
   );
 }
