1eb9fc2b88c1eb70e5250d359cd17b234f2f2322
JunedKhan101 <k.sonuc101@gmail.com>
2021-08-25 12:59:31 +0530

    Changed resume and About component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,9 +5,10 @@ export default function About() {
   return (
 	<div className="about" id="about">
 		<h2>About Myself</h2>
-		<p className="description">As a Developer, I've committed myself to lifelong learning.</p>
-		<p className="description">In my college life, I became passionate about computers and how they work.</p>
-		<p className="description">I am curious about new and promising technologies such as AI, Machine Learning and Blockchain</p>
+		<p className="description">As a Developer, I've committed myself to lifelong learning</p>
+		<p className="description">I'm passionate about computers and how they work.</p>
+		<p className="description">In the past few years. I've build projects, learned about technologies that empower most of the companies, I have always some idea in mind that I'm working on.</p>
+		<p className="description">I'm looking forward to promising technologies such as ML, AI, Blockchain in the foreseeable future.</p>
 	</div>
   );
 }
--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -4,14 +4,14 @@ import "../css/intro.css";
 export default function Intro() {
   return (
   	<div className="intro-container">
-  		<div className="profile" id="profile">
+  		<div className="profile" id="intro">
 		    <img
 		      className="profileimg"
 		      src="static/profileimg2.jpg"
 		      alt="profileimg"
 		    />
   		</div>
-		<div className="intro" id="intro">
+		<div className="intro">
 			<h1>Hi, I'm Juned Khan</h1>
 			<h1>I'm a Web Developer</h1>
 		</div>
