1fab19e9c371bf61815e60fefca12fc9f7bd45b8
JunedKhan101 <k.sonuc101@gmail.com>
2021-09-23 20:24:44 +0530

    changed profile image, polished code in Intro component

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -4,17 +4,19 @@ import "../css/intro.css";
 export default function Intro() {
   return (
   	<div className="intro-container" id="intro">
-  		<div className="profile">
-		    <img
-		      className="profileimg"
-		      src="static/profileimg2.jpg"
-		      alt="profileimg"
-		    />
-  		</div>
-		<div className="intro">
-			<h1>Hi, I'm Juned Khan</h1>
-			<h1>I'm a Web Developer</h1>
-		</div>
+  		<div className="intro">
+	  		<div className="profile">
+			    <img
+			      className="profileimg"
+			      src="static/profileimg.png"
+			      alt="profileimg"
+			    />
+	  		</div>
+				<div className="intro-text">
+					<h1>Hi, I'm Juned Khan;</h1>
+					<h1>I'm a Software Enginner;</h1>
+				</div>
+			</div>
 	</div>
   );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -18,10 +18,10 @@ export default function NavBar() {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav style={navstyle} className="mx-auto my-auto">
+          <Nav.Link className="mynav-links" style={linkstyle} href="static/JunedKhan.pdf" target="_blank">Resume</Nav.Link>
           <Nav.Link className="mynav-links" style={linkstyle} href="#about">About</Nav.Link>
           <Nav.Link className="mynav-links" style={linkstyle} href="#projects">Projects</Nav.Link>
           <Nav.Link className="mynav-links" style={linkstyle} href="#skills">Skills</Nav.Link>
-          <Nav.Link className="mynav-links" style={linkstyle} href="static/JunedKhan.pdf" target="_blank">Resume</Nav.Link>
           <Nav.Link className="mynav-links" style={linkstyle} href="#contact">Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
