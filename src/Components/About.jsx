c2d591a62e4a5fe94539f80f68278d0f3e4147bc
JunedKhan101 <k.sonuc101@gmail.com>
2021-09-24 16:51:55 +0530

    Changed About Myself description

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,10 +5,12 @@ export default function About() {
   return (
 		<div className="about" id="about">
 			<h2>About Myself</h2>
-			<p className="description">As a Developer, I've committed myself to lifelong learning</p>
-			<p className="description">I'm passionate about computers and how they work.</p>
-			<p className="description">In the past few years. I've build projects, learned about technologies that empower most of the companies, I have always some idea in mind that I'm working on.</p>
-			<p className="description">I'm looking forward to promising technologies such as ML, AI, Blockchain in the foreseeable future.</p>
+			<p className="description">Lifelong learner, Introvert, Inquisitive, Geek, Nerd and Bookworm.</p>
+			<p className="description">I did a Bachelor degree in Information Technology from University of Mumbai, and graduated in August 2021.</p>
+			<p className="description">I like to build projects, I'm always working on one behind the scenes.<br />
+																 I'm currently teaching myself and reserching about Machine Learning, Web Scraping and Automation to build a project.</p>
+			<p className="description">I really enjoy learning and programming has taught me that there's no end to it.</p>
+			<p className="description">Bucket list: Networking, Security, Hardware, Electronics, Low level programming, Embedded Systems, ML and AI.</p>
 		</div>
   );
 }
