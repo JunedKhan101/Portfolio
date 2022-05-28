f8bbaaaad07399cec7fcc6140ecc4685e04cef30
Juned Khan <junedkhanc101@gmail.com>
2022-05-28 15:35:07 +0530

    Added themes in localStorage to allow the browser to remember themes

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import Main from "./Main";
 import "../css/app.css";
 
@@ -6,10 +6,30 @@ export const ThemeContext = createContext(null);
 
 function App() {
   const [themeflag, setThemeFlag] = useState(false); // false for light, true for dark theme
-  var theme = themeflag ? "dark" : "light";
+  var theme = themeflag ? "dark" : "light"; // string representation
   const toggleTheme = () => {
     setThemeFlag(themeflag => !themeflag);
+
+    var localtheme = localStorage.getItem("theme");
+
+    if (localtheme === "light")
+      localStorage.setItem("theme", "dark");
+    else if (localtheme === "dark")
+      localStorage.setItem("theme", "light");
   }
+  useEffect(() => {
+    if (localStorage) {
+      var localtheme = localStorage.getItem("theme");
+
+      if (!localtheme)
+        localStorage.setItem("theme", "light");
+      else if (localtheme === "light")
+        setThemeFlag(false);
+      else if (localtheme === "dark")
+        setThemeFlag(true);
+    }
+  }, []);
+
   var html = document.getElementsByTagName("html")[0];
   var body = document.getElementsByTagName("body")[0];
   if (theme === "dark" && themeflag === true) {
