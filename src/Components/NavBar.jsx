dbf29133da832a26e88f7588662ab547e37b051f
Juned Khan <junedkhanc101@gmail.com>
2023-08-14 19:19:48 +0530

    added offcanvas navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Switch from "react-switch";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import { ThemeContext } from "./App";
 import "../css/navbar.css";
 
 export default function NavBar() {
 	const { themeflag, theme, toggleTheme } = useContext(ThemeContext);
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	function getRelativeURL() {
+		const currentURL = window.location.href;
+		const parsedURL = new URL(currentURL);
+		return parsedURL.pathname || "/";
+	}
+	const textColorStyle = {
+		color: theme === "dark" ? "#EEE" : "",
+	};
+	var currentURL = getRelativeURL();
 	return (
 		<>
 			<Navbar
@@ -16,6 +31,10 @@ export default function NavBar() {
 				variant={theme}
 			>
 				<Container>
+					<Navbar.Toggle
+						aria-controls="responsive-navbar-nav"
+						onClick={handleShow}
+					/>
 					<div className="brand-container">
 						<Navbar.Brand className="nav-brand" href="/">
 							Juned Khan
@@ -47,7 +66,6 @@ export default function NavBar() {
 							</label>
 						</Nav.Link>
 					</div>
-					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 					<Navbar.Collapse id="responsive-navbar-nav">
 						<Nav className="mx-auto my-auto my-nav">
 							<Nav.Link
@@ -93,8 +111,61 @@ export default function NavBar() {
 							</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
+					<Offcanvas
+						show={show}
+						onHide={handleClose}
+						className={theme === "dark" ? "bg-dark" : "bg-white"}
+					>
+						<Offcanvas.Header
+							closeButton
+							closeVariant={theme === "dark" ? "white" : null}
+						>
+							<Offcanvas.Title style={textColorStyle}>
+								<Navbar.Brand className="nav-brand" href="/">
+									Juned Khan
+								</Navbar.Brand>
+							</Offcanvas.Title>
+						</Offcanvas.Header>
+						<Offcanvas.Body>
+							<Navbar
+								className={theme === "dark" ? "bg-dark" : "bg-white"}
+								variant={theme}
+							>
+								<Nav
+									className={
+										theme === "dark"
+											? "bg-dark offcanvas-nav flex-column"
+											: "bg-white offcanvas-nav flex-column"
+									}
+								>
+									<Nav.Link
+										className="mynav-links resume-nav-link"
+										href="static/JunedKhan.pdf"
+										target="_blank"
+									>
+										Resume
+									</Nav.Link>
+									<Nav.Link className="mynav-links" href="/blog">
+										Blog
+									</Nav.Link>
+									<Nav.Link className="mynav-links" href="/code-snippets">
+										Code Snippets
+									</Nav.Link>
+									<Nav.Link className="mynav-links" href="/gear">
+										Gear
+									</Nav.Link>
+								</Nav>
+							</Navbar>
+						</Offcanvas.Body>
+					</Offcanvas>
 				</Container>
-				<Container className="custom-nav-container">
+				<Container
+					className={
+						currentURL === "/"
+							? "custom-nav-container"
+							: "d-none custom-nav-container"
+					}
+				>
 					<div className="container-wrapper d-flex flex-column align-items-center justify-content-center w-100">
 						<p className="page-nav-info">On this page:&nbsp;</p>
 						<Nav className="my-auto flex-row custom-nav justify-content-around">
