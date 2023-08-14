d7e9f202c494e6e527e3f1004aca527f42603dff
Juned Khan <junedkhanc101@gmail.com>
2023-08-14 15:15:07 +0530

    added custom navbar for on page navigation

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -8,45 +8,39 @@ import "../css/app.css";
 export const ThemeContext = createContext(null);
 
 function App() {
-  const [themeflag, setThemeFlag] = useState(false); // false for light, true for dark theme
-  var theme = themeflag ? "dark" : "light"; // string representation
-  const toggleTheme = () => {
-    setThemeFlag(themeflag => !themeflag);
+	const [themeflag, setThemeFlag] = useState(false); // false for light, true for dark theme
+	var theme = themeflag ? "dark" : "light"; // string representation
+	const toggleTheme = () => {
+		setThemeFlag((themeflag) => !themeflag);
 
-    var localtheme = localStorage.getItem("theme");
+		var localtheme = localStorage.getItem("theme");
 
-    if (localtheme === "light")
-      localStorage.setItem("theme", "dark");
-    else if (localtheme === "dark")
-      localStorage.setItem("theme", "light");
-  }
-  useEffect(() => {
-    if (localStorage) {
-      var localtheme = localStorage.getItem("theme");
+		if (localtheme === "light") localStorage.setItem("theme", "dark");
+		else if (localtheme === "dark") localStorage.setItem("theme", "light");
+	};
+	useEffect(() => {
+		if (localStorage) {
+			var localtheme = localStorage.getItem("theme");
 
-      if (!localtheme)
-        localStorage.setItem("theme", "light");
-      else if (localtheme === "light")
-        setThemeFlag(false);
-      else if (localtheme === "dark")
-        setThemeFlag(true);
-    }
-  }, []);
+			if (!localtheme) localStorage.setItem("theme", "light");
+			else if (localtheme === "light") setThemeFlag(false);
+			else if (localtheme === "dark") setThemeFlag(true);
+		}
+	}, []);
 
-  var html = document.getElementsByTagName("html")[0];
-  var body = document.getElementsByTagName("body")[0];
-  if (theme === "dark" && themeflag === true) {
-    html.style.backgroundColor = "#1b1e21";
-    body.style.backgroundColor = "#1b1e21";
-    body.style.color = "#EEE";
-  }
-  else if (theme === "light" && themeflag === false) {
-    html.style.backgroundColor = "#FFFFFF";
-    body.style.backgroundColor = "#FFFFFF";
-    body.style.color = "#000000";
-  }
+	var html = document.getElementsByTagName("html")[0];
+	var body = document.getElementsByTagName("body")[0];
+	if (theme === "dark" && themeflag === true) {
+		html.style.backgroundColor = "#1b1e21";
+		body.style.backgroundColor = "#1b1e21";
+		body.style.color = "#EEE";
+	} else if (theme === "light" && themeflag === false) {
+		html.style.backgroundColor = "#FFFFFF";
+		body.style.backgroundColor = "#FFFFFF";
+		body.style.color = "#000000";
+	}
 
-  // Get all sections that have an ID defined
+	// Get all sections that have an ID defined
 	const sections = document.querySelectorAll("section[id]");
 
 	// Add an event listener listening for scroll
@@ -57,7 +51,7 @@ function App() {
 		let scrollY = window.scrollY;
 
 		// Now we loop through sections to get height, top and ID values for each
-		sections.forEach(current => {
+		sections.forEach((current) => {
 			const sectionHeight = current.offsetHeight;
 			const sectionTop = current.offsetTop - 62;
 			var sectionId = current.getAttribute("id");
@@ -66,24 +60,28 @@ function App() {
 			- To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
 			*/
 			if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-				document.querySelector(".my-nav a[href*=" + sectionId + "]").classList.add("active");
+				document
+					.querySelector(".custom-nav a[href*=" + sectionId + "]")
+					.classList.add("active");
 			} else {
-				document.querySelector(".my-nav a[href*=" + sectionId + "]").classList.remove("active");
-        document.querySelector(".my-nav .resume-nav-link").classList.remove("active");
+				document
+					.querySelector(".custom-nav a[href*=" + sectionId + "]")
+					.classList.remove("active");
+				// document.querySelector(".custom-nav .resume-nav-link").classList.remove("active");
 			}
 		});
 	}
-  return (
-    <ThemeContext.Provider value={{themeflag, theme, toggleTheme}}>
-      <div className="app" id={theme}>
-        <NavBar />
-        <Routes>
-          <Route exact path='/' element={<Main />}></Route>
-          <Route exact path='/blog' element={<Blog />}></Route>
-        </Routes>
-	    </div>
-    </ThemeContext.Provider>
-  );
+	return (
+		<ThemeContext.Provider value={{ themeflag, theme, toggleTheme }}>
+			<div className="app" id={theme}>
+				<NavBar />
+				<Routes>
+					<Route exact path="/" element={<Main />}></Route>
+					<Route exact path="/blog" element={<Blog />}></Route>
+				</Routes>
+			</div>
+		</ThemeContext.Provider>
+	);
 }
 
 export default App;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,75 +5,116 @@ import { ThemeContext } from "./App";
 import "../css/navbar.css";
 
 export default function NavBar() {
-  const {themeflag, theme, toggleTheme} = useContext(ThemeContext);
-  return (
-    <Navbar className="navbar-container" fixed="top" collapseOnSelect expand="lg" variant={theme}>
-      <Container>
-        <Navbar.Brand className="nav-brand" href="#intro">Juned Khan</Navbar.Brand>&nbsp;
-        <Nav.Link className="mynav-links dark-mode-toggler-mobile">
-            <label>
-              <Switch onChange={toggleTheme} 
-                checked={themeflag}
-                width={60}
-                offColor="#fecea8"
-                onColor="#142d4c"
-                offHandleColor="#f96d00"
-                onHandleColor="#ffffff" 
-                uncheckedIcon={
-                  <img alt="sun" src="https://img.icons8.com/color/28/000000/sun--v1.png"/>
-                } 
-                checkedIcon={
-                  <img alt="moon" src="https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/28/000000/external-moon-magic-and-fairy-tale-icongeek26-linear-colour-icongeek26.png"/>
-                }
-                />
-            </label>
-            </Nav.Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mx-auto my-auto my-nav">
-            <Nav.Link
-              className="mynav-links resume-nav-link"
-              href="static/JunedKhan.pdf"
-              target="_blank"
-            >
-              Resume
-            </Nav.Link>
-            <Nav.Link className="mynav-links" href="#about">
-              About
-            </Nav.Link>
-            <Nav.Link
-              className="mynav-links"
-              href="#projects"
-            >
-              Projects
-            </Nav.Link>
-            <Nav.Link className="mynav-links" href="#skills">
-              Skills
-            </Nav.Link>
-            <Nav.Link className="mynav-links" href="#contact">
-              Contact
-            </Nav.Link>
-            <Nav.Link className="mynav-links dark-mode-toggler">
-            <label>
-              <Switch onChange={toggleTheme} 
-                checked={themeflag}
-                width={60}
-                offColor="#fecea8"
-                onColor="#142d4c"
-                offHandleColor="#f96d00"
-                onHandleColor="#ffffff" 
-                uncheckedIcon={
-                  <img alt="sun" src="https://img.icons8.com/color/28/000000/sun--v1.png"/>
-                } 
-                checkedIcon={
-                  <img alt="moon" src="https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/28/000000/external-moon-magic-and-fairy-tale-icongeek26-linear-colour-icongeek26.png"/>
-                }
-                />
-            </label>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+	const { themeflag, theme, toggleTheme } = useContext(ThemeContext);
+	return (
+		<>
+			<Navbar
+				className="navbar-container flex-column"
+				fixed="top"
+				collapseOnSelect
+				expand="lg"
+				variant={theme}
+			>
+				<Container>
+					<Navbar.Brand className="nav-brand" href="#intro">
+						Juned Khan
+					</Navbar.Brand>
+					&nbsp;
+					<Nav.Link className="mynav-links dark-mode-toggler-mobile">
+						<label>
+							<Switch
+								onChange={toggleTheme}
+								checked={themeflag}
+								width={60}
+								offColor="#fecea8"
+								onColor="#142d4c"
+								offHandleColor="#f96d00"
+								onHandleColor="#ffffff"
+								uncheckedIcon={
+									<img
+										alt="sun"
+										src="https://img.icons8.com/color/28/000000/sun--v1.png"
+									/>
+								}
+								checkedIcon={
+									<img
+										alt="moon"
+										src="https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/28/000000/external-moon-magic-and-fairy-tale-icongeek26-linear-colour-icongeek26.png"
+									/>
+								}
+							/>
+						</label>
+					</Nav.Link>
+					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+					<Navbar.Collapse id="responsive-navbar-nav">
+						<Nav className="mx-auto my-auto my-nav">
+							<Nav.Link
+								className="mynav-links resume-nav-link"
+								href="static/JunedKhan.pdf"
+								target="_blank"
+							>
+								Resume
+							</Nav.Link>
+							<Nav.Link className="mynav-links" href="#about">
+								About
+							</Nav.Link>
+							<Nav.Link className="mynav-links" href="#projects">
+								Projects
+							</Nav.Link>
+							<Nav.Link className="mynav-links" href="#skills">
+								Skills
+							</Nav.Link>
+							<Nav.Link className="mynav-links" href="#contact">
+								Contact
+							</Nav.Link>
+							<Nav.Link className="mynav-links dark-mode-toggler">
+								<label>
+									<Switch
+										onChange={toggleTheme}
+										checked={themeflag}
+										width={60}
+										offColor="#fecea8"
+										onColor="#142d4c"
+										offHandleColor="#f96d00"
+										onHandleColor="#ffffff"
+										uncheckedIcon={
+											<img
+												alt="sun"
+												src="https://img.icons8.com/color/28/000000/sun--v1.png"
+											/>
+										}
+										checkedIcon={
+											<img
+												alt="moon"
+												src="https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/28/000000/external-moon-magic-and-fairy-tale-icongeek26-linear-colour-icongeek26.png"
+											/>
+										}
+									/>
+								</label>
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+				<Container className="custom-nav-container">
+					<div className="container-wrapper d-flex flex-column align-items-center justify-content-center w-100">
+						{/* <p className="page-nav-info">On this page:&nbsp;</p> */}
+						<Nav className="my-auto flex-row custom-nav justify-content-around">
+							<Nav.Link className="mynav-links" href="#about">
+								About
+							</Nav.Link>
+							<Nav.Link className="mynav-links" href="#projects">
+								Projects
+							</Nav.Link>
+							<Nav.Link className="mynav-links" href="#skills">
+								Skills
+							</Nav.Link>
+							<Nav.Link className="mynav-links" href="#contact">
+								Contact
+							</Nav.Link>
+						</Nav>
+					</div>
+				</Container>
+			</Navbar>
+		</>
+	);
 }
