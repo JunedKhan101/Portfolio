cad7e9f8df50c8f82b77e6ba43ac7c59804b658f
Juned Khan <junedkhanc101@gmail.com>
2023-08-22 14:15:44 +0530

    added output dir and removed comments code from vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-// import viteTsconfigPaths from 'vite-tsconfig-paths';
-// import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.join(__dirname),
   plugins: [react()],
+  build:{
+    outDir: "build"
+  }
 });
