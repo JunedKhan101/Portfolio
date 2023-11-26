1f367cbbe56bc58e1989e8ef22a5388e2ee2aa20
Juned Khan <junedkhanc101@gmail.com>
2023-11-26 16:11:42 +0530

    removed profile image from intro section and added typing animation

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -1,19 +1,28 @@
+import { TypeAnimation } from 'react-type-animation';
 import "../css/intro.css";
 
 export default function Intro() {
 	return (
 		<div className="intro-container" id="intro">
 			<div className="intro">
-				<div className="profile">
+				{/* <div className="profile">
 					<img
 						className="profileimg"
 						src="/static/newprofileimg.png"
 						alt="profileimg"
 					/>
-				</div>
+				</div> */}
 				<div className="intro-text">
-					<h1>Hi, I'm Juned Khan;</h1>
-					<h1>I'm a Software Enginner;</h1>
+					<h1 className="display-1">	
+						<TypeAnimation
+							sequence={[
+								`Hi, I'm Juned Khan;\nI'm a Software Enginner;`,
+							]}
+							speed={50}
+							style={{ whiteSpace: 'pre-line'}}
+							repeat={3}
+						/>
+					</h1>
 				</div>
 			</div>
 		</div>
