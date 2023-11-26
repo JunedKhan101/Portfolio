869d7ab70057285f40e5f12817090138d0e8cbe7
Juned Khan <junedkhanc101@gmail.com>
2023-11-26 16:48:17 +0530

    removed unwanted comments

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -5,13 +5,6 @@ export default function Intro() {
 	return (
 		<div className="intro-container" id="intro">
 			<div className="intro">
-				{/* <div className="profile">
-					<img
-						className="profileimg"
-						src="/static/newprofileimg.png"
-						alt="profileimg"
-					/>
-				</div> */}
 				<div className="intro-text">
 					<h1 className="display-1">	
 						<TypeAnimation
