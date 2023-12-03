2ef90bd1be4e53852e64fd09983a3f382518b73c
Juned Khan <junedkhanc101@gmail.com>
2023-12-03 23:19:38 +0530

    optimized blog code, implemented react-copy-to-clipboard

--- src/Components/Blog.tsx
+++ src/Components/Blog.tsx
@@ -1,14 +1,10 @@
-import { useEffect, useState, useContext } from "react";
-import { useParams } from "react-router-dom";
-import ReactMarkdown from "react-markdown";
-import { createBucketClient } from "@cosmicjs/sdk";
-import { ThemeContext } from "./App";
-import "../css/blog.css";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { useEffect, useState } from "react";
 import { BlogObject } from "../types/cosmicObj";
+import Renderer from "./Renderer";
+import { useParams } from "react-router-dom";
+import { createBucketClient } from "@cosmicjs/sdk";
 import { Helmet } from "react-helmet";
+import "../css/blog.css";
 
 export default function Blog() {
 	var { slug } = useParams();
@@ -25,7 +21,6 @@ export default function Blog() {
 			createdat: '',
 		},
 	});
-	const { theme } = useContext(ThemeContext) as { theme: string };
 	useEffect(() => {
 		const getBlogPost = async () => {
 			const cosmic = createBucketClient({
@@ -59,61 +54,6 @@ export default function Blog() {
 		};
 		getBlogPost();
 	}, [slug]);
-	useEffect(() => {
-		addCopyButton(); // Call the function to add copy buttons after rendering
-	}, [cosmicObj]);
-	function createCopyButton(codeEl: HTMLElement) {
-		const button = document.createElement("button");
-		button.classList.add("prism-copy-button");
-		var svgs =
-		"<svg class='bi bi-clipboard copy-svg' xmlns='http://www.w3.org/2000/svg' width='16' height='21' fill='currentColor' viewBox='0 0 16 16'><path d='M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z'/><path d='M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z'/></svg><svg class='bi bi-check2 copied-svg' xmlns='http://www.w3.org/2000/svg' width='16' height='21' fill='currentColor' viewBox='0 0 16 16'><path d='M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z'/></svg>";
-		button.insertAdjacentHTML("beforeend", svgs);
-
-		const handleCopy = () => {
-			if (!button.classList.contains("active")) {
-				button.classList.add("active");
-				let copysvg = button.querySelector(
-					".copy-svg"
-				) as HTMLElement | null;
-				let copiedsvg = button.querySelector(
-					".copied-svg"
-				) as HTMLElement | null;
-				if (copiedsvg) {
-					copiedsvg.style.display = "block";
-				}
-
-				if (copysvg) {
-					copysvg.style.display = "none";
-				}
-
-				navigator.clipboard.writeText(codeEl.textContent || "");
-				button.disabled = true;
-
-				setTimeout(() => {
-					button.classList.remove("active");
-					button.disabled = false;
-					if (copiedsvg) {
-						copiedsvg.style.display = "none";
-					}
-					if (copysvg) {
-						copysvg.style.display = "block";
-					}
-				}, 3000);
-			}
-		}
-		button.addEventListener("click", handleCopy);
-		return button;
-	}
-	function addCopyButton() {
-		const allPres = document.querySelectorAll("pre");
-		for (const pre of allPres) {
-			if (pre && !pre.querySelector(".prism-copy-button")) {
-				const code = pre.firstElementChild as HTMLElement;
-				var btn = createCopyButton(code);
-				pre.appendChild(btn);
-			}
-		}
-	}
 	const renderBlogContent = () => {
 		if (isLoading) {
 			return (
@@ -139,7 +79,6 @@ export default function Blog() {
 				const year = date.getFullYear();
 				formattedCreatedAtDate = `${day} ${monthNames[month]} ${year}`;
 			}
-
 			return (
 				<>
 					<Helmet>
@@ -179,50 +118,15 @@ export default function Blog() {
 							:
 							null
 						}
-						<ReactMarkdown
-							children={cosmicObj.metadata.content ? cosmicObj.metadata.content : ''}
-							components={{
-								code({
-									node,
-									inline,
-									className,
-									children,
-									...props
-								}) {
-									const match = /language-(\w+)/.exec(
-										className || ""
-									);
-									return !inline && match ? (
-										<SyntaxHighlighter
-											{...props}
-											children={String(children).replace(
-												/\n$/,
-												""
-											)}
-											style={
-												theme === "dark"
-													? oneDark
-													: oneLight
-											}
-											language={match[1]}
-											PreTag="div"
-										/>
-									) : (
-										<code {...props} className={className}>
-											{children}
-										</code>
-									);
-								},
-							}}
-						/>
 					</div>
 				</>
 			);
 		}
 	};
 	return (
-		<section className="blog container" id="blog">
+		<section className="blog container relative" id="blog">
 			{renderBlogContent()}
+			<Renderer content={cosmicObj.metadata.content ? cosmicObj.metadata.content : '' } />
 		</section>
 	);
 }
