925d0498d669e09e35f97dca9e430fb9184bd8e3
Juned Khan <junedkhanc101@gmail.com>
2023-12-04 11:45:46 +0530

    optimized code: added two more svg components

--- src/Components/Renderer.tsx
+++ src/Components/Renderer.tsx
@@ -2,12 +2,14 @@
 import { useContext } from "react";
 import { ThemeContext } from "./App";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import ClipBoardSVG from "./svg/ClipBoardSVG";
+import CheckSVG from "./svg/CheckSVG";
+import "../css/renderer.css";
 
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
-import "../css/renderer.css";
 
 export default function CopyButton({ content }: any) {
 	const { theme } = useContext(ThemeContext) as { theme: string };
@@ -33,27 +35,8 @@ export default function CopyButton({ content }: any) {
 										onClick={handleCopy}
 										className="prism-copy-button"
 									>
-										<svg
-											className="bi bi-clipboard copy-svg"
-											xmlns="http://www.w3.org/2000/svg"
-											width="16"
-											height="21"
-											fill="currentColor"
-											viewBox="0 0 16 16"
-										>
-											<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
-											<path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
-										</svg>
-										<svg
-											className="bi bi-check copied-svg"
-											xmlns="http://www.w3.org/2000/svg"
-											width="16"
-											height="21"
-											fill="currentColor"
-											viewBox="0 0 16 16"
-										>
-											<path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
-										</svg>
+										<ClipBoardSVG />
+										<CheckSVG />
 									</button>
 								</CopyToClipboard>
 								<SyntaxHighlighter
