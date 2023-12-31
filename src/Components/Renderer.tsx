ac73cfce749f6adff40ab5b05bc4bdbc13c32eba
Juned Khan <junedkhanc101@gmail.com>
2023-12-31 18:55:20 +0530

    updated all packages

--- src/Components/Renderer.tsx
+++ src/Components/Renderer.tsx
@@ -1,5 +1,5 @@
 // This component renders the blog content in markdown and highlights the syntax of the code
-import { useContext, lazy } from "react";
+import { useContext, lazy, useRef,forwardRef } from "react";
 import { ThemeContext } from "./App";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 const ClipBoardSVG = lazy(() => import("./svg/ClipBoardSVG"));
@@ -12,9 +12,9 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-export default function CopyButton({ content }: any) {
+export default function Renderer({ content }: any) {
+	// const syntaxHighlighterRef = useRef<SyntaxHighlighter>(null);
 	const { theme } = useContext(ThemeContext) as { theme: string };
-
 	const handleCopy = (event: React.MouseEvent<HTMLButtonElement>) => {
 		var currentBtn = event.currentTarget;
         currentBtn.classList.add("active");
@@ -27,11 +27,12 @@ export default function CopyButton({ content }: any) {
 			children={content ? content : ""}
 			rehypePlugins={[rehypeRaw]}
 			components={{
-				code({ node, inline, className, children, ...props }: {node: any, inline: any, className: any, children: any}) {
+				// @ts-ignore
+				code({ node, className, children, ...props }) {
 					const match = /language-(\w+)/.exec(className || "");
-					return !inline && match ? (
+					return match ? (
 						<>
-							<CopyToClipboard text={String(children[0])}>
+							<CopyToClipboard text={String(children)}>
 								<button
 									onClick={handleCopy}
 									className="prism-copy-button"
@@ -52,6 +53,7 @@ export default function CopyButton({ content }: any) {
 								language={match[1]}
 								showLineNumbers={true}
 								PreTag="div"
+								ref={(ref) => {}}
 							/>
 						</>
 					) : (
