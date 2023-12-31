1010fa3a524f714ba89a50fdcaaa68552c796161
Juned Khan <junedkhanc101@gmail.com>
2023-12-31 19:43:56 +0530

    updated old react markdown code to new one

--- src/Components/Renderer.tsx
+++ src/Components/Renderer.tsx
@@ -1,34 +1,33 @@
 // This component renders the blog content in markdown and highlights the syntax of the code
-import { useContext, lazy, useRef,forwardRef } from "react";
+import { useContext, lazy, useRef, forwardRef } from "react";
 import { ThemeContext } from "./App";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 const ClipBoardSVG = lazy(() => import("./svg/ClipBoardSVG"));
 const CheckSVG = lazy(() => import("./svg/CheckSVG"));
 import "../css/renderer.css";
 
-import ReactMarkdown from "react-markdown";
+import Markdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-export default function Renderer({ content }: any) {
-	// const syntaxHighlighterRef = useRef<SyntaxHighlighter>(null);
+export default function Renderer({ content }: { content: string }) {
 	const { theme } = useContext(ThemeContext) as { theme: string };
 	const handleCopy = (event: React.MouseEvent<HTMLButtonElement>) => {
 		var currentBtn = event.currentTarget;
-        currentBtn.classList.add("active");
-        setTimeout(() => {
-            currentBtn.classList.remove("active");
-        }, 3000);
+		currentBtn.classList.add("active");
+		setTimeout(() => {
+			currentBtn.classList.remove("active");
+		}, 3000);
 	};
 	return (
-		<ReactMarkdown
+		<Markdown
 			children={content ? content : ""}
 			rehypePlugins={[rehypeRaw]}
 			components={{
-				// @ts-ignore
-				code({ node, className, children, ...props }) {
+				code(props) {
+					const { children, className, node, ...rest } = props;
 					const match = /language-(\w+)/.exec(className || "");
 					return match ? (
 						<>
@@ -42,20 +41,15 @@ export default function Renderer({ content }: any) {
 								</button>
 							</CopyToClipboard>
 							<SyntaxHighlighter
-								children={String(children).replace(
-									/\n$/,
-									""
-								)}
-								style={
-									theme === "dark" ? oneDark : oneLight
-								}
+								children={String(children).replace(/\n$/, "")}
+								style={theme === "dark" ? oneDark : oneLight}
 								language={match[1]}
 								showLineNumbers={true}
 								PreTag="div"
 							/>
 						</>
 					) : (
-						<code {...props} className={className}>
+						<code {...rest} className={className}>
 							{children}
 						</code>
 					);
